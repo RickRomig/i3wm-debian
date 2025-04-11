74567bffc64ae91c8a65491dc40a913cac961bda
Richard Romig <rick.romig@gmail.com>
2025-04-11 08:50:11 -0400

    utils.sh: initial commit. Utility functions for installing packages and cloning repositories.

---
+++ utils.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+###############################################################################
+# Script Name  : utils.sh
+# Description  : Utility functions for installing packages for Debian i3WM
+# Dependencies : None
+# Arguments    : None
+# Author       : Copyright © 2025, Richard B. Romig, Mosfanet
+# Email        : rick.romig@gmail.com | rick.romig@mymetronet.com
+# Created      : 10 Apr 2025
+# Last updated : 10 Apr 2025
+# Version      : 1.0.25100
+# Comments     :
+# TODO (Rick)  :
+# License      : GNU General Public License, version 2.0
+###############################################################################
+
+# Function to check if a package is installed
+is_installed() {
+	dpkg -l "$1" &> /dev/null && return 0 || return 1
+}
+
+# Function to install packages if not already installed
+install_packages() {
+  local packages=("$@")
+  local to_install=()
+
+  for pkg in "${packages[@]}"; do
+    if ! is_installed "$pkg"; then
+    	to_install+=("$pkg")
+    fi
+  done
+
+  if [ ${#to_install[@]} -ne 0 ]; then
+    for pkg in "${to_install[@]}"; do
+    	echo "Installing $pkg..."
+    	apt-get install -yy "$pkg"
+    done
+  fi
+}
+
+clone_repos() {
+	local gitea_url="http://192.168.0.16:3000/Nullifidian"
+	local repos=(configs scripts)
+	local dl_dir="$HOME/downloads"
+
+	for repo in "${repos[@]}"; do
+		if [[ -d "$dl_dir/$repo" ]]; then
+			echo "$repo repository already exists."
+		else
+			git clone "$gitea_url/$repo.git" "$dl_dir/$repo"
+		fi
+	done
+}
