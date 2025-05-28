94c657b2155e6a33f4f0c050f3e29ce3a9d943e2
Richard Romig <rick.romig@gmail.com>
2025-05-28 14:01:14 -0400

    configs.sh: apply_configs(): corrected copying of notify-send icons

--- configs.sh
+++ configs.sh
@@ -53,7 +53,7 @@ apply_configs() {
 	esac
   cp -rv "$cloned_dir/i3" "$config_dir/"
   cp -rv "$cloned_dir/polybar" "$config_dir/"
-	cp -rv "$cloned_dir/icons" "$HOME/"
+	cp -rv "$cloned_dir/icons/*" "$HOME/.icons/"
 	cp -v "$HOME/i3wm-debian/dmconf.sh" "$HOME/.local/bin/"
 	cp -v "$cloned_dir/local/leave.txt" "$HOME/.local/share/doc/"
 	sudo cp -v "$cloned_dir"/sleep.conf /etc/systemd/
