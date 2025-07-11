e7c6708f23d7425947d153eb43a45bb72732c909
Richard Romig <rick.romig@gmail.com>
2025-07-11 14:19:57 -0400

    utils.sh: added GPL license info & terms and conditons.

--- utils.sh
+++ utils.sh
@@ -7,11 +7,22 @@
 # Author       : Copyright © 2025, Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.com
 # Created      : 10 Apr 2025
-# Last updated : 03 Jun 2025
-# Version      : 1.2.25154
+# Last updated : 11 Jul 2025
+# Version      : 1.3.25192
 # Comments     : Sourced in run.sh
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2.0
+# License URL  : https://github.com/RickRomig/i3wm-debian/blob/main/README.md
+###############################################################################
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
 ###############################################################################
 
 # Function to check if a package is installed
