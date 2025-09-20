8470588fbca87a64311ae593acb78821c30e6dd5
Richard Romig <rick.romig@gmail.com>
2025-09-20 16:33:09 -0400

    nerdfonts.sh: removed CascadiaCode from array; removed sudo from mkdir & tar commands

--- nerdfonts.sh
+++ nerdfonts.sh
@@ -7,7 +7,7 @@
 # Author       : Copyright © 2025, Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.com
 # Created      : 10 Apr 2025
-# Last updated : 11 Jul 2025
+# Last updated : 20 Sep 2025
 # Comments     : Run this script after run.sh.
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2.0
@@ -35,15 +35,14 @@ install_nerd_fonts() {
   symbols_font="SymbolsNerdFontMono-Regular.ttf"
   symbols_archive="NerdFontsSymbolsOnly.tar.xz"
   font_repo="https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0"
-  # font_dir="/usr/local/share/fonts"
   font_dir="$HOME/.local/share/fonts"
   [[ -d "$font_dir" ]] || sudo mkdir -p "$font_dir"
-  fonts=( "CascadiaCode" "FiraCode" "Go-Mono" "Hack" "Inconsolata" "Iosevka" "JetBrainsMono" "Mononoki" "RobotoMono" "SourceCodePro" )
+  fonts=( "FiraCode" "Go-Mono" "Hack" "Inconsolata" "Iosevka" "JetBrainsMono" "Mononoki" "RobotoMono" "SourceCodePro" )
   for font in "${fonts[@]}"; do
     printf "\e[03mInstalling %s ...\e[0m\n" "$font"
     wget -P "$tmp_dir" "$font_repo/$font.tar.xz"
-    sudo mkdir -p "$font_dir/$font/"
-    sudo tar -xvf "$tmp_dir/$font.tar.xz" -C "$font_dir/$font/"
+    mkdir -p "$font_dir/$font/"
+    tar -xvf "$tmp_dir/$font.tar.xz" -C "$font_dir/$font/"
   done
   wget -P "$tmp_dir" "$font_repo/$symbols_archive"
   tar -xvf "$tmp_dir/$symbols_archive" "$font_dir/$symbols_font"
@@ -54,13 +53,13 @@ install_nerd_fonts() {
 main() {
   local script version
 	script="${0##*/}"
-  version="1.2.25192"
+  version="1.3.25263"
   tmp_dir=$(mktemp -d) || { printf "\e[91mERROR:\e[0m: Failed to create temporary directory." >&2; exit 1; }
   trap cleanup EXIT
   install_nerd_fonts
 	echo "-----------------"
 	echo "$script $version"
-  printf "Run \e[93mconfigs.sh\e[0m to setup configuratino files and complete the installation.\n"
+  printf "Run \e[93mconfigs.sh\e[0m to setup configuration files and complete the installation.\n"
   exit
 }
 
